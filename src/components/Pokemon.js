ce18b189b7f8e0b09af3983d93afe442ecba43e4
KodeAndre <andre.kodehode@gmail.com>
2021-06-29 10:58:08 +0200

    v1

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,8 +3,27 @@ import React from 'react';
 
 export function Pokemon({ pokemon }) {
     return (
-        <p>
-            {pokemon.name}
-        </p>
+        <div className="pokemon">
+            <div>
+                <div className="pokemon__name">
+                    <h2>{pokemon.name}</h2>
+                </div>
+
+                <div className="pokemon__meta">
+                    <p>Max HP: {pokemon.maxHP}</p>
+                    <p>Max CP: {pokemon.maxCP}</p>
+                </div>
+
+                <div className="pokemon__image">
+                    <img src={pokemon.image}></img>
+                </div>
+
+                <div className="pokemon__attacks">
+                    {pokemon.attacks.special.slice(0, 3).map(attacks => 
+                    <p key={`${attacks.name}-${attacks.damage}`}>Name: {attacks.name} Power: {attacks.damage}</p>
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
